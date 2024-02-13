7af2c27762c84f0c55762a20f0387b440e1addce
Nolb226 <hn26677@gmail.com>
2024-02-13 22:32:00 +0700

    update

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -5,6 +5,7 @@ import route from '../apps/index.js';
 import handler from '../libraries/ErrorManagement/handler/index.js';
 import { getDataBaseConfig } from './db-config.js';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 class App {
 	public app: express.Application;
@@ -16,7 +17,7 @@ class App {
 
 	private config(): void {
 		dotenv.config();
-
+		this.app.use(cors());
 		// this.app.use(bodyParser.urlencoded({ extended: true }));
 		// this.app.use(bodyParser.json());
 		this.app.use((req, res, next) => {
